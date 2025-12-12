694d314a0b1147b9a968b254042a2c4ccfab7f0b
Willemien Vesseur <vesseurw@gmail.com>
2025-12-12 19:23:24 +0100

    Fix duplicate supabase variable in supervisor portal

--- public/static/js/supervisor-portal.js
+++ public/static/js/supervisor-portal.js
@@ -1,6 +1,3 @@
-const SUPABASE_URL = 'https://ninkkvffhvkxrrxddgrz.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pbmtrdmZmaHZreHJyeGRkZ3J6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5OTc2NTcsImV4cCI6MjA3OTU3MzY1N30.Kq6jojYu5Hopmtzmdqwc9dwUyIZBOm7c27N-OCv1aCM';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 let students = [];
 let companies = [];
