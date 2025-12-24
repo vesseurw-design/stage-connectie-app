fe3c080a55f26ec2768203e9e4aa6b0f1f299747
Willemien Vesseur <vesseurw@gmail.com>
2025-12-24 12:25:07 +0100

    fix: Restore Supabase client initialization in admin-companies.js

--- public/static/js/admin-companies.js
+++ public/static/js/admin-companies.js
@@ -3,8 +3,9 @@
  * Met branche functionaliteit
  */
 
-// Use the global authenticated Supabase client from admin-auth.js
-const supabaseClient = window.supabase;
+const SUPABASE_URL = 'https://ninkkvffhvkxrrxddgrz.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pbmtrdmZmaHZreHJyeGRkZ3J6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5OTc2NTcsImV4cCI6MjA3OTU3MzY1N30.Kq6jojYu5Hopmtzmdqwc9dwUyIZBOm7c27N-OCv1aCM';
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 let allBranches = [];
 let currentFilter = '';
